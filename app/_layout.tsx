c537e98da2a1f556d2a8f12324d59ffaffcfae2a
anyta58 <henriquezvictoria58@gmail.com>
2025-03-13 17:48:17 -0600

    primer commit

---
+++ app/_layout.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "expo-router";
+import '../global.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+const RootLayout = () => {
+
+  const queryClient = new QueryClient();
+  
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack 
+        screenOptions={{
+          headerShown: false
+        }}
+      />
+    </QueryClientProvider>
+  )
+}
+
+export default RootLayout
